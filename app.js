6feeee1418c206e3d219537c3d71b706540dc9b1
Difficins <a.kolomijec@seznam.cz>
2024-01-10 11:52:30 +0100

    Displaying library data done

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const coolRouter = require("./routes/cool");
+//Import routes for "catalog" area of site
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
 
@@ -22,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/users/cool", coolRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
